f2b435442ca69c3283de38b52878efb50a6ae457
JeizonF <jeizonfilho@gmail.com>
2026-05-22 15:51:55 -0300

    problema pra minha dupla

--- atv.py
+++ atv.py
@@ -1,1 +1,5 @@
 #criacao da atv.py
+
+#escreva um programa que le 3 numeros reais a,b e c,
+#representando os coeficientes de uma equeção do segundo grau. 
+#seu programa deve calcular as raizes da equação e exibir conforme os exemplos de saida
